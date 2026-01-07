316b5bac88330346efb5ef4d2c82ff986c4bb95c
Softvex <softvex1@gmail.com>
2026-01-07 04:32:02 +0000

    add somthing moving elemts like floating  here

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,8 +4,43 @@ import Image from 'next/image';
 
 export function HeroSection() {
   return (
-    <section className="relative w-full py-24 md:py-32 lg:py-40">
-      <div className="container mx-auto grid grid-cols-1 items-center gap-12 text-center">
+    <section className="relative w-full overflow-hidden py-24 md:py-32 lg:py-40">
+       <div
+        className="blob -top-1/4 left-1/4 h-[500px] w-[500px] bg-cyan-400/50"
+        style={
+          {
+            '--tw-translate-x': '100px',
+            '--tw-translate-y': '50px',
+            '--tw-scale-x': '1.2',
+            animationDuration: '25s'
+          } as React.CSSProperties
+        }
+      ></div>
+      <div
+        className="blob -bottom-1/4 right-1/4 h-[500px] w-[500px] bg-teal-400/50"
+        style={
+          {
+            '--tw-translate-x': '-80px',
+            '--tw-translate-y': '-120px',
+            '--tw-scale-x': '1.1',
+             animationDelay: '5s',
+             animationDuration: '30s'
+          } as React.CSSProperties
+        }
+      ></div>
+       <div
+        className="blob top-1/4 right-1/2 h-[300px] w-[300px] bg-blue-400/50"
+        style={
+          {
+            '--tw-translate-x': '120px',
+            '--tw-translate-y': '-100px',
+            '--tw-scale-x': '0.9',
+             animationDelay: '10s',
+             animationDuration: '35s'
+          } as React.CSSProperties
+        }
+      ></div>
+      <div className="container relative mx-auto grid grid-cols-1 items-center gap-12 text-center">
         <div className="space-y-6 col-span-1">
           <h1 className="font-headline text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
             <span className="text-reveal-wrapper">
