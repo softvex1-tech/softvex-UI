91d052e565a68573831abac96d4e60c5e2f5c4db
Softvex <softvex1@gmail.com>
2026-01-08 04:49:12 +0000

    File changes

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -19,7 +19,7 @@ type CareerFormState = {
 function getGoogleSheet() {
   const serviceAccountAuth = new JWT({
     email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    key: process.env.GOOGLE_PRIVATE_KEY,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
 
